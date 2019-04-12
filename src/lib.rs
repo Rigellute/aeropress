e6b80bb030aa919988e7ee2409a3e4818ef91c46
Rigellute <rigellute@gmail.com>
2019-04-12 11:09:31 +0100

    Add restart logic, improve instructions

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,9 @@ use std::time::Duration;
 use yew::services::{ConsoleService, IntervalService, Task};
 use yew::{html, Callback, Component, ComponentLink, Html, Renderable, ShouldRender};
 
+// While debugging it can be useful to speed through the timer - but this value should be 1000ms
+const TIMER_SPEED: u64 = 1000;
+
 pub struct Model {
     interval: IntervalService,
     callback_tick: Callback<()>,
@@ -22,63 +25,65 @@ pub enum Msg {
     CancelTimer,
     IncrementStep,
     ToggleStart,
+    Restart,
 }
 
 pub struct Step {
     display_at_count: i32,
     instruction: &'static str,
     next_instruction: &'static str,
-    requires_action: Msg,
+    on_click: Msg,
 }
 
+// TODO: Put these instructions into a json file
 const INSTRUCTIONS: [Step; 8] = [
     Step {
         display_at_count: 0,
-        instruction: "Set your AeroPress inverted and wet double paper filters with hot water.",
+        instruction: "Set your AeroPress inverted and wet a paper filters with hot water.",
         next_instruction: "",
-        requires_action: Msg::IncrementStep,
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 0,
-        instruction: "Set your water temperature at 81 celsius degrees, put the coffee in and fill up with 70 grams of water.",
+        instruction: "Set your water temperature at 81 celsius degrees, put in one scoop of the coffee and fill up with 70 grams of water.",
         next_instruction: "",
-        requires_action: Msg::IncrementStep,
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 0,
         instruction: "Start timer and swirl your AeroPress around for 15 seconds.",
         next_instruction: "",
-        requires_action: Msg::StartTimer,
+        on_click: Msg::StartTimer,
     },
     Step {
         display_at_count: 15,
         instruction: "Fill up with the remaining amount of water.",
         next_instruction: "Wait until 60 seconds, then put the cap on.",
-        requires_action: Msg::IncrementStep,
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 60,
         instruction: "Put the cap on",
         next_instruction: "Wait until 80 seconds, then invert.",
-        requires_action: Msg::IncrementStep,
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 80,
         instruction: "Invert the areopress.",
         next_instruction: "Wait until 90 seconds and start pluging.",
-        requires_action: Msg::IncrementStep,
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 90,
         instruction: "Start plunging.",
-        next_instruction: "Aim to finish with 15 seconds.",
-        requires_action: Msg::IncrementStep,
+        next_instruction: "Aim to finish within 15 seconds.",
+        on_click: Msg::IncrementStep,
     },
     Step {
         display_at_count: 105,
-        instruction: "Done!",
+        instruction: "Done! Enjoy your coffee.",
         next_instruction: "",
-        requires_action: Msg::CancelTimer,
+        on_click: Msg::CancelTimer,
     }
 ];
 
@@ -107,6 +112,11 @@ impl Component for Model {
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
+            Msg::Restart => {
+                self.state.has_started = false;
+                self.state.count = 0;
+                self.state.current_step = 0;
+            }
             Msg::ToggleStart => {
                 self.state.has_started = !self.state.has_started;
             }
@@ -114,9 +124,11 @@ impl Component for Model {
                 self.state.current_step += 1;
             }
             Msg::StartTimer => {
-                let handle = self
-                    .interval
-                    .spawn(Duration::from_secs(1), self.callback_tick.clone());
+                self.console.log("Timer has started");
+                let handle = self.interval.spawn(
+                    Duration::from_millis(TIMER_SPEED),
+                    self.callback_tick.clone(),
+                );
                 self.job = Some(Box::new(handle));
             }
             Msg::Tick => {
@@ -129,14 +141,14 @@ impl Component for Model {
                     }
                     None => {}
                 };
+
+                if self.state.current_step == INSTRUCTIONS.len() - 1 {
+                    self.cancel_timer();
+                }
                 self.state.count += 1;
             }
             Msg::CancelTimer => {
-                if let Some(mut task) = self.job.take() {
-                    task.cancel();
-                };
-                self.state.count = 0;
-                self.job = None;
+                self.cancel_timer();
             }
         }
         true
@@ -148,14 +160,10 @@ impl Renderable<Model> for Model {
         html! {
             <div class="container",>
                 <section class="header",>
-                    <h1 class="title",>{ "Aeropress timer" } </h1>
+                    <h1 class="title",>{ "AeroPress Timer" } </h1>
                     <i data-feather="coffee",></i>
-                    <p>{ "Brew the perfect cup every time" }</p>
+                    <p>{ "Brew the perfect cup every time." }</p>
                 </section>
-                <div class="row-buttons",>
-                    <button onclick=|_| Msg::ToggleStart,>{ "Begin" }</button>
-                    <button onclick=|_| Msg::CancelTimer ,>{ "Stop" }</button>
-                </div>
                 {self.view_instruction()}
             </div>
         }
@@ -163,48 +171,71 @@ impl Renderable<Model> for Model {
 }
 
 impl Model {
-    fn view_instruction(&self) -> Html<Model> {
-        let empty_node: Html<Self> = html! {
-            <div></div>
+    fn cancel_timer(&mut self) {
+        if let Some(mut task) = self.job.take() {
+            task.cancel();
         };
+        self.state.count = 0;
+        self.job = None;
+    }
+
+    fn restart_button(&self) -> Html<Model> {
+        let is_done = self.state.current_step == INSTRUCTIONS.len() - 1;
+        if !self.job.is_some() && is_done {
+            html! {
+                <button onclick=|_| Msg::Restart,>{ "Restart" }</button>
+            }
+        } else {
+            empty_node()
+        }
+    }
+
+    fn view_instruction(&self) -> Html<Model> {
+        if !self.state.has_started {
+            return html! {
+                <button onclick=|_| Msg::ToggleStart,>{ "Begin" }</button>
+            };
+        }
 
         let timer_html: Html<Self> = match self.job.is_some() {
             true => html! {
                 <div class="timer",>{self.state.count}</div>
             },
-            false => empty_node,
+            false => empty_node(),
         };
 
         if self.state.has_started {
             let step = &INSTRUCTIONS.get(self.state.current_step);
             match step {
                 Some(some_step) => {
-                    let step_button = match some_step.requires_action {
+                    let step_button = match some_step.on_click {
                         Msg::IncrementStep => {
                             html! { <button onclick=|_| Msg::IncrementStep,>{"Next"}</button>}
                         }
                         Msg::StartTimer => {
                             html! { <button onclick=|_| Msg::StartTimer,>{"Start Timer"}</button>}
                         }
-                        _ => html! {</>},
+                        _ => empty_node(),
                     };
                     html! {
                         <>
-                            <p>{ some_step.instruction }</p>
-                            <p>{ some_step.next_instruction }</p>
                             {step_button}
+                            {self.restart_button()}
+                            <p>{ some_step.instruction } {" "} { some_step.next_instruction }</p>
                             {timer_html}
                         </>
                     }
                 }
-                None => html! {
-                    <div></div>
-                },
+                None => empty_node(),
             }
         } else {
-            html! {
-                <div></div>
-            }
+            empty_node()
         }
+    }
+}
+
+fn empty_node() -> Html<Model> {
+    html! {
+        <div></div>
     }
 }
